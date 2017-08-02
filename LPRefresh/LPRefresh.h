3b3beb5413cbf1aee348aae0523aa76fcde8d972
litt1e-p <free9fw@gmail.com>
2017-08-02 14:13:25 +0800

    fix module header

--- LPRefresh/LPRefresh.h
+++ LPRefresh/LPRefresh.h
@@ -24,6 +24,6 @@
 #define Refresh_h
 
 #import "UIScrollView+RefreshControl.h"
-#import <UIScrollView-InfiniteScroll/UIScrollView+InfiniteScroll.h>
+#import <UIScrollView_InfiniteScroll/UIScrollView+InfiniteScroll.h>
 
 #endif /* Refresh_h */
